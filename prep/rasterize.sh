f659503eaee302a5fb1877507b4a455bee42a051
Chris Arderne <chris@rdrn.me>
2019-04-01 12:56:25 +0200

    Update rasterize script

--- prep/rasterize.sh
+++ prep/rasterize.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# Rasterize all in dir using targets for shape and extents
+# First arg: directory containing files to rasterize
+# Second arg: directory for output
 
-for f in COL
-do
-	targ=~/data/targets/$f.tif
-	gdal_rasterize -at -burn 1 -a_nodata 0 -init 0 -ts $(./getres.sh $targ) -te $(./getextents.sh $targ)  ~/data/grid_vec/$f.gpkg ~/data/grid/$f.tif
+for f in $1/*; do
+    name=$(echo $f | sed -r "s/.+\/(.+)\..+/\1/");
+	targ=~/data/targets/$name.tif;
+	gdal_rasterize -at -burn 1 -a_nodata 0 -init 0 -ts $(./getres.sh $targ) -te $(./getextents.sh $targ) $f $2/$name.tif
 done
 
