2dd90350173b3bcdfbd0f2a16e659c038aa752ba
Chris Arderne <chris@rdrn.me>
2019-04-14 17:32:07 +0200

    Improve logging behaviour

--- runner.py
+++ runner.py
@@ -43,7 +43,6 @@ percentile = cfg["options"]["percentile"]
 ntl_threshold = cfg["options"]["ntl_threshold"]
 threads = cfg["options"]["threads"]
 raise_errors = False
-debug = False
 
 admin = gpd.read_file(admin_in)
 
@@ -89,9 +88,6 @@ def spawn(tool, countries):
 
 
 def targets(country):
-    log = "targets.txt"
-
-    # Setup
     this_scratch = scratch / f"targets_{country}"
     ntl_out = this_scratch / "ntl"
     ntl_merged_out = this_scratch / "ntl_merged.tif"
@@ -99,7 +95,7 @@ def targets(country):
     targets_out = get_filename(targets_dir, country)
 
     try:
-        print("Targets start", country)
+        print(f"Targets\tstart\t{country}")
         this_scratch.mkdir(parents=True, exist_ok=True)
         aoi = admin.loc[admin[code] == country]
         buff = aoi.copy()
@@ -107,14 +103,8 @@ def targets(country):
 
         # Clip NTL rasters and calculate nth percentile values
         gf.clip_rasters(ntl_in, ntl_out, buff)
-        if debug:
-            print("Rasters clipped")
         raster_merged, affine = gf.merge_rasters(ntl_out, percentile=percentile)
-        if debug:
-            print("Merged")
         gf.save_raster(ntl_merged_out, raster_merged, affine)
-        if debug:
-            print("Saved")
 
         # Apply filter to NTL
         ntl_filter = gf.create_filter()
@@ -125,61 +115,49 @@ def targets(country):
             upsample_by=1,
             threshold=ntl_threshold,
         )
-        if debug:
-            print("Prepared")
         gf.save_raster(ntl_thresh_out, ntl_thresh, affine)
-        if debug:
-            print("Saved")
 
         # Clip to actual AOI
         targets, affine, _ = gf.clip_raster(ntl_thresh_out, aoi)
-        if debug:
-            print("Clipped again")
         gf.save_raster(targets_out, targets, affine)
+        msg = f"Targets\tDONE\t{country}"
 
-        msg = f"Done {country}"
     except Exception as e:
-        msg = f"Failed {country} -- {e}"
+        msg = f"Targets\tFAILED\t{country}\t{e}"
         if raise_errors:
             raise
     finally:
-        # Clean up
         shutil.rmtree(this_scratch)
         print(msg)
-        with open(log, "a") as f:
-            print(msg, file=f)
+        if log:
+            with open(log, "a") as f:
+                print(msg, file=f)
 
 
 def costs(country):
-    log = "costs.txt"
-
-    # Setup
     targets_in = get_filename(targets_dir, country)
     costs_in = get_filename("costs_vec", country, ext="gpkg")
     costs_out = get_filename(costs_dir, country)
 
     try:
-        print("Costs start", country)
+        print(f"Costs\tstart\t{country}")
         aoi = admin.loc[admin[code] == country]
-
         roads_raster, affine = gf.prepare_roads(costs_in, aoi, targets_in)
         gf.save_raster(costs_out, roads_raster, affine)
-        msg = f"Done {country}"
+
+        msg = f"Costs\tDONE\t{country}"
     except Exception as e:
-        msg = f"Failed {country} -- {e}"
+        msg = f"Costs\tFAILED\t{country}\t{e}"
         if raise_errors:
             raise
     finally:
-        # Clean up
         print(msg)
-        with open(log, "a") as f:
-            print(msg, file=f)
+        if log:
+            with open(log, "a") as f:
+                print(msg, file=f)
 
 
 def dijk(country):
-    log = "dijk.txt"
-
-    # Setup
     this_scratch = scratch / f"dijk_{country}"
     dist_out = this_scratch / "dist.tif"
     targets_in = get_filename(targets_dir, country)
@@ -187,7 +165,7 @@ def dijk(country):
     guess_out = get_filename(guess_dir, country)
 
     try:
-        print("Dijk start", country)
+        print(f"Dijk\tstart\t{country}")
         this_scratch.mkdir(parents=True, exist_ok=True)
 
         targets, costs, start, affine = gf.get_targets_costs(targets_in, costs_in)
@@ -196,53 +174,47 @@ def dijk(country):
         guess, affine = gf.threshold(dist_out)
         guess_skel = gf.thin(guess)
         gf.save_raster(guess_out, guess_skel, affine)
-        msg = f"Done {country}"
+
+        msg = f"Dijk\tDONE\t{country}"
     except Exception as e:
-        msg = f"Failed {country} -- {e}"
+        msg = f"Dijk\tFAILED\t{country}\t{e}"
         if raise_errors:
             raise
     finally:
-        # Clean up
         shutil.rmtree(this_scratch)
         print(msg)
-        with open(log, "a") as f:
-            print(msg, file=f)
+        if log:
+            with open(log, "a") as f:
+                print(msg, file=f)
 
 
 def vector(country):
-    log = "vector.txt"
-
-    # Setup
     guess_in = get_filename(guess_dir, country)
     guess_vec_out = get_filename(vector_dir, country, ext="gpkg")
 
     try:
-        print("Vec start", country)
-
+        print(f"Vector\tstart\t{country}")
         guess_gdf = gf.raster_to_lines(guess_in)
         guess_gdf.to_file(guess_vec_out, driver="GPKG")
-        msg = f"Done {country}"
+
+        msg = f"Vector\tDONE\t{country}"
     except Exception as e:
-        msg = f"Failed {country} -- {e}"
+        msg = f"Vector\tFAILED\t{country}\t{e}"
         if raise_errors:
             raise
     finally:
-        # Clean up
         print(msg)
-        with open(log, "a") as f:
-            print(msg, file=f)
+        if log:
+            with open(log, "a") as f:
+                print(msg, file=f)
 
 
 def pop_elec(country):
-    log = "access.txt"
-
-    # Setup
     targets_in = get_filename(targets_dir, country)
     pop_elec_out = get_filename(pop_elec_dir, country)
 
     try:
-        print("Access start", country)
-
+        print(f"PopElec\tstart\t{country}")
         aoi = admin.loc[admin[code] == country]
         access = aoi[["total", "urban", "rural"]].iloc[0].to_dict()
 
@@ -252,27 +224,24 @@ def pop_elec(country):
         pop_elec, access_model_total = ea.estimate(pop, urban, ntl, targets, access)
         gf.save_raster(pop_elec_out, pop_elec, affine, crs)
 
-        msg = f"{country},real: {access['total']:.2f},model: {access_model_total:.2f}"
+        msg = f"PopElec\tDONE\t{country}\t\treal: {access['total']:.2f}\tmodel: {access_model_total:.2f}"
     except Exception as e:
-        msg = f"Failed {country} -- {e}"
+        msg = f"PopElec\tFAILED\t{country}\t{e}"
         if raise_errors:
             raise
     finally:
         print(msg)
-        with open(log, "a") as f:
-            print(msg, file=f)
+        if log:
+            with open(log, "a") as f:
+                print(msg, file=f)
 
 
 def local(country):
-    log = "local.txt"
-
-    # Setup
     pop_elec_in = get_filename(pop_elec_dir, country)
     lv_out = get_filename(local_dir, country)
 
     try:
-        print("Local start", country)
-
+        print(f"Local\tstart\t{country}")
         pop_elec_rd = rasterio.open(pop_elec_in)
         pop_elec = pop_elec_rd.read(1)
         affine = pop_elec_rd.transform
@@ -281,31 +250,43 @@ def local(country):
         costs = ea.apply_lv_length(pop_elec)
         gf.save_raster(lv_out, costs, affine, crs)
 
-        msg = f"Done {country}"
+        msg = f"Local\tDONE\t{country}"
     except Exception as e:
-        msg = f"Failed {country} -- {e}"
+        msg = f"Local\tFAILED\t{country}\t{e}"
         if raise_errors:
             raise
     finally:
         print(msg)
-        with open(log, "a") as f:
-            print(msg, file=f)
+        if log:
+            with open(log, "a") as f:
+                print(msg, file=f)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("tool")
     parser.add_argument("--countries")
-    parser.add_argument("-r", action="store_true")  # Whether to raise errors
-    parser.add_argument("-d", action="store_true")  # Whether to print debug messages
-    parser.add_argument("--targets_dir")
-    parser.add_argument("--costs_dir")
-    parser.add_argument("--guess_dir")
-    parser.add_argument("--vector_dir")
-    parser.add_argument("--pop_elec_dir")
-    parser.add_argument("--local_dir")
-    parser.add_argument("--percentile")
-    parser.add_argument("--ntl_threshold")
+    parser.add_argument("--targets_dir", default=targets_dir)
+    parser.add_argument("--costs_dir", default=costs_dir)
+    parser.add_argument("--guess_dir", default=guess_dir)
+    parser.add_argument("--vector_dir", default=vector_dir)
+    parser.add_argument("--pop_elec_dir", default=pop_elec_dir)
+    parser.add_argument("--local_dir", default=local_dir)
+    parser.add_argument("--percentile", default=percentile, type=int)
+    parser.add_argument("--ntl_threshold", default=ntl_threshold, type=float)
+    parser.add_argument(
+        "-r",
+        "--raise_errors",
+        action="store_true",
+        default=False,
+        help="Whether to raise errors",
+    )
+    parser.add_argument(
+        "-l",
+        "--log",
+        default=None,
+        help="If supplied, logs will be written to this file",
+    )
     args = parser.parse_args()
 
     switch = {
@@ -321,42 +302,22 @@ if __name__ == "__main__":
     if func is None:
         sys.exit(f"Option {args.tool} not supported")
 
+    countries = None
     if args.countries:
         if "," in args.countries:
             countries = args.countries.split(",")
         else:
             countries = [args.countries]
-    else:
-        countries = None
 
-    if args.r:
-        raise_errors = True
-
-    if args.d:
-        debug = True
-
-    if args.targets_dir:
-        targets_dir = args.targets_dir
-
-    if args.costs_dir:
-        costs_dir = args.costs_dir
-
-    if args.guess_dir:
-        guess_dir = args.guess_dir
-
-    if args.vector_dir:
-        vector_dir = args.vector_dir
-
-    if args.pop_elec_dir:
-        pop_elec_dir = args.pop_elec_dir
-
-    if args.local_dir:
-        local_dir = args.local_dir
-
-    if args.percentile:
-        percentile = int(args.percentile)
-
-    if args.ntl_threshold:
-        ntl_threshold = float(args.ntl_threshold)
+    targets_dir = args.targets_dir
+    costs_dir = args.costs_dir
+    guess_dir = args.guess_dir
+    vector_dir = args.vector_dir
+    pop_elec_dir = args.pop_elec_dir
+    local_dir = args.local_dir
+    percentile = args.percentile
+    ntl_threshold = args.ntl_threshold
+    raise_errors = args.raise_errors
+    log = args.log
 
     spawn(func, countries)
